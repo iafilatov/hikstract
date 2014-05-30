fe6e15da3879f1ec75aba4bdf62b7e1c01192fc9
esyn <ia.filatov@gmail.com>
2014-05-30 21:18:51 +0300

    Add config validation

--- config.py
+++ config.py
@@ -3,10 +3,6 @@ import configparser
 _CFGFILE = 'config.cfg'
 
 _DEFAULTS = {
-            'main': {
-                        'data_dir': './in',
-                        'output_dir': './out',
-                        },
             'advanced': {
                          'avconv_args': '-c:v copy -v error',
                          'db_file': 'index.db',
@@ -14,11 +10,16 @@ _DEFAULTS = {
                          },
             }
 
+_MANDATORY = {
+              'main': ('data_dir', 'output_dir'),
+              }
+
 
 class CFG(configparser.ConfigParser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.configure()
+        self.validate()
         
     def configure(self, cfgfile=_CFGFILE):
         self._load_defaults()
@@ -37,5 +38,23 @@ class CFG(configparser.ConfigParser):
         with open(cfgfile, 'w') as f:
             self.write(f)
             
+    def validate(self):
+        for sec, keys in _MANDATORY.items():
+            if sec not in self:
+                raise ConfigError('Section [{}] must be present in'
+                                  ' config file'.format(sec))
+            for k in keys:
+                if k not in self[sec]:
+                    raise ConfigError('Parameter `{}` must be present in'
+                                      ' section [{}]'.format(k, sec))
+
+
+class ConfigError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    
+    def __str__(self):
+        return self.msg
+                
 
 cfg = CFG()
