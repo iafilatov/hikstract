074a531de1d4c6a3dacac64f1d0aed051cc0568d
esyn <ia.filatov@gmail.com>
2014-05-31 19:54:48 +0300

    Add a few docstrings

--- utils.py
+++ utils.py
@@ -1,27 +1,25 @@
-from collections import defaultdict
 from itertools import chain, islice
-import json
 import struct
 
 
 def islice_from(it, start):
+    '''Given a sequence 0..start..N, iterate over start..N'''
     return islice(it, start, len(it))
 
 
 def full_circle(it, start):
+    '''Given a sequence 0..start..N, iterate over start..N, then over 0..start'''
     return chain(islice_from(it, start), islice(it, start))
 
 
-def log_byte(b):
-    return '{:02x}'.format(b)
-
-
 def log_short(s):
+    '''Return a string representation of s as little-endian unsigned short'''
     p = struct.pack('<H', s)
     return ''.join('{:02x}'.format(b) for b in p)
 
 
 def log_int(i):
+    '''Return a string representation of s as little-endian unsigned int'''
     p = struct.pack('<I', i)
     return ''.join('{:02x}'.format(b) for b in p)
     
